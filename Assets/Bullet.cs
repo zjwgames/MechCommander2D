4ba40a6510dcc133830896811ad7e2fc44cb698e
ZJW Games <zzbeest@gmail.com>
2023-01-16 16:32:11 +0000

    Updated player sprite and bullet prefab

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public float maxLifetime = 1f;
+    private float lifetime = 0f;
+
+    void Update()
+    {
+        lifetime += 1 * Time.deltaTime;
+        if (lifetime > maxLifetime) Destroy(gameObject);
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         Destroy(gameObject);
